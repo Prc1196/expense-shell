f661a66ceb27a07a03924374287da0527e300cb9
Pradeepreddyc <prc1196@gmail.com>
2025-01-15 12:26:48 +0530

    expense shell

--- backend.sh
+++ backend.sh
@@ -51,7 +51,7 @@ else
     echo -e "expense user already exists ... $Y SKIPPING $N"
 fi
 
-mkdir /app
+mkdir -p /app
 VALIDATE $? "Creating app directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
